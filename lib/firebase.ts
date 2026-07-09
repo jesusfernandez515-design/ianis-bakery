9383571aa3dfd201e8af6ebda4c1f1e03c118b8b
jesusfernandez515-design <jesusfernandez515@gmail.com>
2026-07-09 12:06:21 -0400

    Configure Firebase connection

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDrCUvH4jDLDIr9pLQcr-fEzPixHmR0jjg",
+  authDomain: "ianis-bakery.firebaseapp.com",
+  projectId: "ianis-bakery",
+  storageBucket: "ianis-bakery.firebasestorage.app",
+  messagingSenderId: "1033970694489",
+  appId: "1:1033970694489:web:4c8a7bf325f558f1d7f448",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default app;
