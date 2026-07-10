328fdd869bf5889038631e765c919c54270d71db
jesusfernandez515-design <jesusfernandez515@gmail.com>
2026-07-10 13:19:08 -0400

    Fix Firebase web app configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,16 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrCUvH4jDLDIr9pLQcr-fEzPixHmR0jjg",
-  authDomain: "ianis-bakery.firebaseapp.com",
-  projectId: "ianis-bakery",
-  storageBucket: "ianis-bakery.firebasestorage.app",
-  messagingSenderId: "1033970694489",
-  appId: "1:1033970694489:web:4c8a7bf325f558f1d7f448",
+  apiKey: "PEGA_AQUI_LA_API_KEY_EXACTA_DE_FIREBASE",
+  authDomain: "PEGA_AQUI_EL_AUTH_DOMAIN",
+  projectId: "PEGA_AQUI_EL_PROJECT_ID",
+  storageBucket: "PEGA_AQUI_EL_STORAGE_BUCKET",
+  messagingSenderId: "PEGA_AQUI_EL_MESSAGING_SENDER_ID",
+  appId: "PEGA_AQUI_EL_APP_ID",
 };
 
 const app =
-  getApps().length > 0 ? getApps()[0] : initializeApp(firebaseConfig);
+  getApps().length > 0
+    ? getApps()[0]
+    : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
